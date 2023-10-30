f3c179a207131d4cefb8599ef367d04f90cca1f1
FrancescaPicoco <francesca.picoco33@gmail.com>
2023-10-30 21:54:06 +0100

    almost

--- main.js
+++ main.js
@@ -1,15 +1,34 @@
+
+const container = document.getElementById("container"); 
+
 for (let i = 1; i <= 100; i++) {  
 
-    if ( i%3 == 0){
+    if(i%15 == 0) {
+        const element = document.createElement("p");
+        element.innerHTML = `${i}`;
+        element.classList.add("fizzbuzz");
+        container.append(element);
+        console.log("FIZZBUZZ" ,i);
+    } 
+    else if( i%3 == 0){
+        const element = document.createElement("p");
+        element.innerHTML = `${i}`;
+        element.classList.add("fizz");
+        container.append(element);
         console.log("FIZZ " ,i);
     }
     else if (i%5 == 0) {
+        const element = document.createElement("p");
+        element.innerHTML = `${i}`;
+        element.classList.add("buzz");
+        container.append(element);
         console.log("BUZZ" ,i);
     }
-     if(i%15 == 0) {
-        console.log("FIZZBUZZ" ,i);
-    }
     else{
+        const element = document.createElement("p");
+        element.innerHTML = `${i}`;
+        element.classList.add("others");
+        container.append(element);
         console.log(i);
     }
 }
