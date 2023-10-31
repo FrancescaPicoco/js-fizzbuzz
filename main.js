155f176170205d4daa9530d77a6559bffb0110e1
FrancescaPicoco <francesca.picoco33@gmail.com>
2023-10-31 14:45:30 +0100

    LAST

--- main.js
+++ main.js
@@ -2,33 +2,35 @@
 const container = document.getElementById("container"); 
 
 for (let i = 1; i <= 100; i++) {  
-
+    const element = document.createElement("p");
     if(i%15 == 0) {
-        const element = document.createElement("p");
+        //const element = document.createElement("p");
         element.innerHTML = `${i}`;
         element.classList.add("fizzbuzz");
-        container.append(element);
+        //container.append(element);
         console.log("FIZZBUZZ" ,i);
     } 
     else if( i%3 == 0){
-        const element = document.createElement("p");
+        //const element = document.createElement("p");
         element.innerHTML = `${i}`;
         element.classList.add("fizz");
-        container.append(element);
+        //container.append(element);
         console.log("FIZZ " ,i);
     }
     else if (i%5 == 0) {
-        const element = document.createElement("p");
+        //const element = document.createElement("p");
         element.innerHTML = `${i}`;
         element.classList.add("buzz");
-        container.append(element);
+        //container.append(element);
         console.log("BUZZ" ,i);
     }
     else{
-        const element = document.createElement("p");
+        //const element = document.createElement("p");
         element.innerHTML = `${i}`;
         element.classList.add("others");
-        container.append(element);
+        //container.append(element);
         console.log(i);
     }
+    container.append(element);
+
 }
